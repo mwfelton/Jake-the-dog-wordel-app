1563745dfd2659027af8f2a3cba9c404bdc7c871
Miles Felton Gerber <mwfelton03@gmail.com>
2025-02-01 13:40:10 +0000

    words app and model - setup stage

--- server/words/models.py
+++ server/words/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Word(models.Model):
+    text = models.CharField(max_length=5, unique=True)  # A 5-letter word
+
+    def __str__(self):
+        return self.text
